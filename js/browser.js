11db738e3b4fa2c9b53cc8c0daeb8364c2b1f0f6
r4sso <r4ssonull@gmail>
2023-02-17 02:29:28 +0800

    ie

--- js/browser.js
+++ js/browser.js
@@ -1,4 +1,6 @@
-if ((navigator.userAgentData && navigator.userAgentData.brands && navigator.userAgentData.brands.some(brand => brand.brand === 'Microsoft Edge' && brand.version >= 12)) || (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0)) {
-    alert('Please note that your browser is not fully compatible with this website. Some features may not work as expected.');
-  }
+$(document).ready(function() {
+    if ((navigator.userAgentData && navigator.userAgentData.brands && navigator.userAgentData.brands.some(brand => brand.brand === 'Microsoft Edge' && brand.version >= 12)) || (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0)) {
+      $('#browser').show();
+    }
+  });
   
