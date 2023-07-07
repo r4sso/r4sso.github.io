c75095cb756d3664185ae44be14bd77d1fda440c
r4sso <r4ssonull@gmail>
2023-07-07 19:25:42 +0800

    Gallery

--- js/browser.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    if ((navigator.userAgentData && navigator.userAgentData.brands && navigator.userAgentData.brands.some(brand => brand.brand === 'Microsoft Edge' && brand.version >= 12)) || (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0)) {
-      $('#browser').show();
-    }
-  });
-  
