6e6512b76f87bb2b0b8c2f5dd87df2f94e2a6f33
r4sso <programmernet106@gmail.com>
2023-10-02 17:59:57 +0800

    Cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,98 @@
-// Function to update the theme attribute
-function updateThemeAttribute(theme) {
-  // Get the element with the data-theme attribute
-  const elementWithTheme = document.querySelector('[data-theme]');
-
+// Function to toggle the theme
+function toggleTheme() {
+  const isDarkMode = document.documentElement.classList.toggle('dark');
+  // Set the color-theme in local storage
+  localStorage.setItem('color-theme', isDarkMode ? 'dark' : 'light');
 }
 
-// Check if the theme has been set via local storage previously
-if (!localStorage.getItem('color-theme')) {
+// Function to handle theme icon visibility
+function handleThemeIcons() {
+  const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+  const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+  // Change the icons inside the button based on the current theme
+  if (document.documentElement.classList.contains('dark')) {
+    themeToggleDarkIcon.classList.add('hidden'); // Add hidden class to dark icon
+    themeToggleLightIcon.classList.remove('hidden'); // Remove hidden class from light icon
+  } else {
+    themeToggleLightIcon.classList.add('hidden'); // Add hidden class to light icon
+    themeToggleDarkIcon.classList.remove('hidden'); // Remove hidden class from dark icon
+  }
+}
+
+// Function to handle mobile menu toggle
+function handleMobileMenu() {
+  const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  // Add click event listener to mobile menu button
+  mobileMenuButton.addEventListener('click', () => {
+    // Toggle the "aria-expanded" attribute
+    const isExpanded = mobileMenuButton.getAttribute('aria-expanded') === 'true';
+    mobileMenuButton.setAttribute('aria-expanded', !isExpanded);
+    
+    // Toggle the "hidden" class on the mobile menu
+    mobileMenu.classList.toggle('hidden');
+  });
+}
+
+// Function to handle scroll to top
+function handleScrollToTop() {
+  window.onscroll = function() { scrollFunction() };
+  
+  function scrollFunction() {
+    const scrollToTopButton = document.getElementById("scroll-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopButton.style.display = "block";
+    } else {
+      scrollToTopButton.style.display = "none";
+    }
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Check if the theme has been set via local storage
+const storedTheme = localStorage.getItem('color-theme');
+if (storedTheme) {
+  // Apply the stored theme to the document element
+  document.documentElement.classList.add(storedTheme);
+
+  // Handle theme icons based on the stored theme
+  handleThemeIcons();
+} else {
   // Get the user's preferred color scheme (dark or light)
   const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
     ? 'dark'
     : 'light';
 
-  // Set the color-theme cookie to the preferred theme
+  // Set the color-theme in local storage to the preferred theme
   localStorage.setItem('color-theme', preferredTheme);
 
   // Apply the preferred theme to the document element
   document.documentElement.classList.add(preferredTheme);
 
-  // Update the data-theme attribute
-  updateThemeAttribute(preferredTheme);
+  // Handle theme icons based on the preferred theme
+  handleThemeIcons();
 }
 
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+// Get theme toggle button
+const themeToggleBtn = document.getElementById('theme-toggle');
 
-// Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark') {
-  themeToggleLightIcon.classList.remove('hidden');
-} else {
-  themeToggleDarkIcon.classList.remove('hidden');
-}
+// Add a click event listener to the theme toggle button
+themeToggleBtn.addEventListener('click', () => {
+  // Toggle the theme
+  toggleTheme();
 
-var themeToggleBtn = document.getElementById('theme-toggle');
-
-themeToggleBtn.addEventListener('click', function () {
-  // toggle icons inside button
-  themeToggleDarkIcon.classList.toggle('hidden');
-  themeToggleLightIcon.classList.toggle('hidden');
-
-  // Toggle the theme when the button is clicked
-  if (document.documentElement.classList.contains('dark')) {
-    document.documentElement.classList.remove('dark');
-    localStorage.setItem('color-theme', 'light');
-
-  } else {
-    document.documentElement.classList.add('dark');
-    localStorage.setItem('color-theme', 'dark');
-  }
+  // Handle theme icons
+  handleThemeIcons();
 });
 
+// Handle mobile menu
+handleMobileMenu();
 
-// Get the mobile menu button element
-const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
-
-// Get the mobile menu element
-const mobileMenu = document.getElementById('mobile-menu');
-
-// Add click event listener to mobile menu button
-mobileMenuButton.addEventListener('click', () => {
-  // Toggle the "aria-expanded" attribute
-  const isExpanded = mobileMenuButton.getAttribute('aria-expanded') === 'true';
-  mobileMenuButton.setAttribute('aria-expanded', !isExpanded);
-  
-  // Toggle the "hidden" class on the mobile menu
-  mobileMenu.classList.toggle('hidden');
-});
-
-2
-// scroll to top button
-window.onscroll = function() { scrollFunction() };
-  
-    function scrollFunction() {
-      var scrollToTopButton = document.getElementById("scroll-to-top");
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopButton.style.display = "block";
-      } else {
-        scrollToTopButton.style.display = "none";
-      }
-    }
-  
-    function scrollToTop() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    };
-
-
+// Handle scroll to top
+handleScrollToTop();
